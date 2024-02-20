07b2fd8adb430753d50f5047a9709e6bdac43165
zaitsev.av <zaitsev.av.12@gmail.com>
2024-02-20 21:41:54 +0300

    feat: added auth and user entities

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,5 @@
+import { JwtModuleOptions } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+export const getJwtConfig = async (
+	configService: ConfigService
+): Promise<JwtModuleOptions> => ({ secret: configService.get('JWT_SECRET') })
---
+++ src/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) {}
+
+	async getById(id: string) {
+		return this.prisma.user.findUnique({
+			where: {
+				id
+			},
+			include: {
+				tasks: true
+			}
+		})
+	}
+}
